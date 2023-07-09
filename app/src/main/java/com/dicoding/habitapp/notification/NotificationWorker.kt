c66fae34f712b156dc871c852cc9591f44937292
daffaya <daffapasya21@gmail.com>
2023-07-09 22:03:51 +0700

    Done TODO 12: Show notif with pending intent

--- app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -1,11 +1,20 @@
 package com.dicoding.habitapp.notification
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
 import com.dicoding.habitapp.utils.HABIT_ID
 import com.dicoding.habitapp.utils.HABIT_TITLE
+import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
+import com.dicoding.habitapp.utils.NOTIF_UNIQUE_WORK
 
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
@@ -17,7 +26,31 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
         val shouldNotify = prefManager.getBoolean(applicationContext.getString(R.string.pref_key_notify), false)
 
         //TODO 12 : If notification preference on, show notification with pending intent
+        if (shouldNotify) {
+            val notificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+            val notificationBuilder =
+                NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID).apply {
+                    setSmallIcon(R.drawable.ic_notifications)
+                    setContentTitle(habitTitle)
+                    setContentText(applicationContext.getString(R.string.notify_content))
+
+                    val intent = Intent(applicationContext, AddHabitActivity::class.java)
+                    setContentIntent(
+                        PendingIntent.getActivity(applicationContext, 0, intent, 0)
+                    )
+                }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID,
+                    NOTIF_UNIQUE_WORK,
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                notificationManager.createNotificationChannel(channel)
+            }
+            notificationManager.notify(habitId, notificationBuilder.build())
+        }
         return Result.success()
     }
 
