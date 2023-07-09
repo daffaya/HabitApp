9dfb701b42abc3220423daab859ba9111df3f452
daffaya <daffapasya21@gmail.com>
2023-07-09 18:15:04 +0700

    Done TODO 1: Define Local Database and adding Object HabitFieldName.kt to handle the field name.

--- app/src/main/java/com/dicoding/habitapp/data/Habit.kt
+++ app/src/main/java/com/dicoding/habitapp/data/Habit.kt
@@ -1,14 +1,24 @@
 package com.dicoding.habitapp.data
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 //TODO 1 : Define a local database table using the schema in app/schema/habits.json
 @Parcelize
+@Entity(tableName = HabitFieldName.TABLE_NAME)
 data class Habit(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = HabitFieldName.COL_ID)
     val id: Int = 0,
+    @ColumnInfo(name = HabitFieldName.COL_TITLE)
     val title: String,
+    @ColumnInfo(name = HabitFieldName.COL_MINUTES_FOCUS)
     val minutesFocus: Long,
+    @ColumnInfo(name = HabitFieldName.COL_START_TIME)
     val startTime: String,
+    @ColumnInfo(name = HabitFieldName.COL_PRIORITY_LEVEL)
     val priorityLevel: String
 ): Parcelable
---
+++ app/src/main/java/com/dicoding/habitapp/data/HabitFieldName.kt
@@ -0,0 +1,11 @@
+package com.dicoding.habitapp.data
+
+//For Reviewer, i'm sorry i add this, i just want to learn to tidying up my projects
+object HabitFieldName {
+    const val TABLE_NAME = "habits"
+    const val COL_ID = "id"
+    const val COL_TITLE = "title"
+    const val COL_MINUTES_FOCUS = "minutesFocus"
+    const val COL_START_TIME = "startTime"
+    const val COL_PRIORITY_LEVEL = "priorityLevel"
+}
